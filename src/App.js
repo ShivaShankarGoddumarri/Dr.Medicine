746446fb393b8747a5829edaf048f9e36484daea
ShivaShankar <ss3177806@gmail.com>
2023-09-27 12:37:49 +0530

    updated db

--- src/App.js
+++ src/App.js
@@ -14,27 +14,27 @@ function App() {
       medication: "Topical Acne Cream (e.g., Benzoyl Peroxide)",
       directions: "Apply a small amount to affected skin areas once daily as directed by your dermatologist.",
     },
-    allergicRhinitis: {
+    AllergicRhinitis: {
       medication: "Loratadine (e.g., Claritin)",
       directions: "Take 1 tablet daily in the morning to manage allergy symptoms.",
     },
-    allergies: {
+    Allergies: {
       medication: "Antihistamines (Ex., Claritin)",
       directions: "Take 1 tablet daily in the morning to manage allergy symptoms.",
     },
-    anxiety: {
+    Anxiety: {
       medication: "Anti-anxiety medication (Ex., Xanax)",
       directions: "Take as prescribed by your healthcare provider to manage anxiety symptoms.",
     },
-    asthmaAttack: {
+    AsthmaAttack: {
       medication: "Inhaler (e.g., Albuterol)",
       directions: "Follow the instructions provided with your inhaler for immediate relief during an asthma attack.",
     },
-    backpain: {
+    Backpain: {
       medication: "Acetaminophen",
       directions: "Take 2 tablets every 4-6 hours.",
     },
-    cold: {
+    Cold: {
       medication: "Coldact",
       directions: "Take 1 tablet every 4-6 hours.",
     },
@@ -42,99 +42,99 @@ function App() {
       medication: "Montelukast + Levocetirizine",
       directions: "Take as prescribed by your healthcare provider to manage cold and sore throat symptoms."
     },
-    constipation: {
+    Constipation: {
       medication: "Fiber supplements (Ex., Metamucil)",
       directions: "Mix 1-2 tablespoons with water and drink once daily to relieve constipation.",
     },
-    cough: {
+    Cough: {
       medication: "Cough syrup",
       directions: "Take 1-2 teaspoons every 4-6 hours as needed.",
     },
-    depression: {
+    Depression: {
       medication: "Antidepressants (Ex., Prozac)",
       directions: "Take as prescribed by your healthcare provider to manage depression.",
     },
-    diarrhea: {
+    Diarrhea: {
       medication: "Imodium",
       directions: "Take 1-2 capsules after each loose bowel movement, up to 4 times a day.",
     },
-    eyeinfection: {
+    EyeInfection: {
       medication: "Eye drops (Ex., Visine)",
       directions: "Apply 1-2 drops to the affected eye(s) every 4-6 hours as needed.",
     },
-    fever: {
+    Fever: {
       medication: "Ibuprofen or Dolo 650",
       directions: "Take 1 tablet every 6-8 hours with food.",
     },
-    FeverAndBodypainsAndInflammation: {
+    FeverAndBodyPainsAndInflammation: {
       medication: "Zerodol-sp",
       directions: "Take 1 tablet every 6-8 hours with food.",
     },
-    headache: {
+    Headache: {
       medication: "Aspirin",
       directions: "Take 1 tablet every 4-6 hours.",
     },
-    hives: {
+    Hives: {
       medication: "Antihistamines (e.g., Benadryl)",
       directions: "Take 1-2 tablets every 4-6 hours as needed for hives relief.",
     },
-    indigestion: {
+    Indigestion: {
       medication: "Antacids (Ex., Tums)",
       directions: "Chew 2-4 tablets as needed for relief from heartburn and indigestion.",
     },
-    insectbite: {
+    InsectBite: {
       medication: "Hydrocortisone cream",
       directions: "Apply a small amount to the bite area for itch relief, up to 3 times a day.",
     },
-    insomnia: {
+    Insomnia: {
       medication: "Melatonin",
       directions: "Take 1-5mg of melatonin 30 minutes before bedtime to improve sleep.",
     },
-    minorburns: {
+    MinorBurns: {
       medication: "Burn Cream (e.g., Neosporin)",
       directions: "Apply a thin layer to the burned area for soothing relief and infection prevention.",
     },
-    migraine: {
+    Migraine: {
       medication: "Sumatriptan",
       directions: "Take 1 tablet at the onset of a migraine headache. Do not exceed 2 tablets in 24 hours.",
     },
-    motionsickness: {
+    MotionSickness: {
       medication: "Dimenhydrinate (e.g., Dramamine)",
       directions: "Take 1-2 tablets 30 minutes before travel to prevent motion sickness.",
     },
-    nausea: {
+    Nausea: {
       medication: "Pepto-Bismol",
       directions: "Take 2 tablespoons every 4-6 hours as needed.",
     },
-    rash: {
+    Rash: {
       medication: "Calamine lotion",
       directions: "Apply a thin layer to the rash as needed for relief from itching and irritation.",
     },
-    runnynose: {
+    RunnyNose: {
       medication: "Antihistamines",
       directions: "Take 1 tablet daily in the evening.",
     },
-    sinusitis: {
+    Sinusitis: {
       medication: "Nasal Decongestant (e.g., Sudafed)",
       directions: "Take 1 tablet every 4-6 hours as needed for nasal congestion relief.",
     },
-    sorethroat: {
+    SoreThroat: {
       medication: "Throat lozenges",
       directions: "Dissolve one lozenge in your mouth as needed, up to 4 times a day.",
     },
-    sprainedankle: {
+    SprainedAnkle: {
       medication: "Over-the-counter pain reliever (e.g., Ibuprofen)",
       directions: "Take 1 tablet every 6-8 hours with food for pain and inflammation.",
     },
-    stomachpain: {
+    StomachPain: {
       medication: "Omee",
       directions: "Take one tablet early in the morning.",
     },
-    sunburn: {
+    Sunburn: {
       medication: "Aloe vera gel",
       directions: "Apply a thin layer to sunburned skin as needed for soothing relief.",
     },
-    toothache: {
+    Toothache: {
       medication: "Dentalgel",
       directions: "Apply a small amount directly to the affected tooth or gum as needed.",
     },
@@ -188,13 +188,16 @@ function App() {
     const medicationText = `MEDICATION RESULTS\nDesigned by Shiva Shankar Goddumarri\n${
       currentDateTime ? currentDateTime.toLocaleString() : ""
     }\n\n` + medications.map((data) => `Symptom: ${data.symptom}\nMedication: ${data.medication}\nDirections: ${data.directions}`).join("\n\n");
-
+  
     navigator.clipboard
       .writeText(medicationText)
-      .then(() => setCopyStatus(true))
+      .then(() => {
+        setCopyStatus(true);
+        setTimeout(() => setCopyStatus(false), 3000);
+      })
       .catch(() => setCopyStatus(false));
   };
-
+  
   return (
     <div className="App">
       <div className="background-image"></div>
