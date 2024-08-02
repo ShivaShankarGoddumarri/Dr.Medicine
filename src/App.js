a4476058815d99cd4086fd5cfbffed39693d5f4a
ShivaShankar <ss3177806@gmail.com>
2024-08-02 12:59:14 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import {
 } from "@mui/material";
 import { Search, Clear, ContentCopy } from "@mui/icons-material";
 import { createFilterOptions } from "@mui/material/Autocomplete";
+import symptomDatabase from "./symptomDatabase";
 
 
 function App() {
@@ -25,159 +26,7 @@ function App() {
   const [copyStatus, setCopyStatus] = useState(false);
   const [isCheckButtonVisible, setIsCheckButtonVisible] = useState(true);
 
-  const symptomDatabase = {
-    AcneBreakout: {
-      medication: "Topical Acne Cream (e.g., Benzoyl Peroxide)",
-      directions:
-        "Apply a small amount to affected skin areas once daily as directed by your dermatologist.",
-    },
-    AllergicRhinitis: {
-      medication: "Loratadine (e.g., Claritin)",
-      directions:
-        "Take 1 tablet daily in the morning to manage allergy symptoms.",
-    },
-    Allergies: {
-      medication: "Antihistamines (Ex., Claritin)",
-      directions:
-        "Take 1 tablet daily in the morning to manage allergy symptoms.",
-    },
-    Anxiety: {
-      medication: "Anti-anxiety medication (Ex., Xanax)",
-      directions:
-        "Take as prescribed by your healthcare provider to manage anxiety symptoms.",
-    },
-    AsthmaAttack: {
-      medication: "Inhaler (e.g., Albuterol)",
-      directions:
-        "Follow the instructions provided with your inhaler for immediate relief during an asthma attack.",
-    },
-    Backpain: {
-      medication: "Acetaminophen",
-      directions: "Take 2 tablets every 4-6 hours.",
-    },
-    Cold: {
-      medication: "Coldact",
-      directions: "Take 1 tablet every 4-6 hours.",
-    },
-    ColdAndSoreThroat: {
-      medication: "Montelukast + Levocetirizine",
-      directions:
-        "Take as prescribed by your healthcare provider to manage cold and sore throat symptoms.",
-    },
-    Constipation: {
-      medication: "Fiber supplements (Ex., Metamucil)",
-      directions:
-        "Mix 1-2 tablespoons with water and drink once daily to relieve constipation.",
-    },
-    Cough: {
-      medication: "Cough syrup",
-      directions: "Take 1-2 teaspoons every 4-6 hours as needed.",
-    },
-    Depression: {
-      medication: "Antidepressants (Ex., Prozac)",
-      directions:
-        "Take as prescribed by your healthcare provider to manage depression.",
-    },
-    Diarrhea: {
-      medication: "Imodium",
-      directions:
-        "Take 1-2 capsules after each loose bowel movement, up to 4 times a day.",
-    },
-    EyeInfection: {
-      medication: "Eye drops (Ex., Visine)",
-      directions:
-        "Apply 1-2 drops to the affected eye(s) every 4-6 hours as needed.",
-    },
-    Fever: {
-      medication: "Ibuprofen or Dolo 650",
-      directions: "Take 1 tablet every 6-8 hours with food.",
-    },
-    FeverAndBodyPainsAndInflammation: {
-      medication: "Zerodol-sp",
-      directions: "Take 1 tablet every 6-8 hours with food.",
-    },
-    Headache: {
-      medication: "Aspirin",
-      directions: "Take 1 tablet every 4-6 hours.",
-    },
-    Hives: {
-      medication: "Antihistamines (e.g., Benadryl)",
-      directions:
-        "Take 1-2 tablets every 4-6 hours as needed for hives relief.",
-    },
-    Indigestion: {
-      medication: "Antacids (Ex., Tums)",
-      directions:
-        "Chew 2-4 tablets as needed for relief from heartburn and indigestion.",
-    },
-    InsectBite: {
-      medication: "Hydrocortisone cream",
-      directions:
-        "Apply a small amount to the bite area for itch relief, up to 3 times a day.",
-    },
-    Insomnia: {
-      medication: "Melatonin",
-      directions:
-        "Take 1-5mg of melatonin 30 minutes before bedtime to improve sleep.",
-    },
-    MinorBurns: {
-      medication: "Burn Cream (e.g., Neosporin)",
-      directions:
-        "Apply a thin layer to the burned area for soothing relief and infection prevention.",
-    },
-    Migraine: {
-      medication: "Sumatriptan",
-      directions:
-        "Take 1 tablet at the onset of a migraine headache. Do not exceed 2 tablets in 24 hours.",
-    },
-    MotionSickness: {
-      medication: "Dimenhydrinate (e.g., Dramamine)",
-      directions:
-        "Take 1-2 tablets 30 minutes before travel to prevent motion sickness.",
-    },
-    Nausea: {
-      medication: "Pepto-Bismol",
-      directions: "Take 2 tablespoons every 4-6 hours as needed.",
-    },
-    Rash: {
-      medication: "Calamine lotion",
-      directions:
-        "Apply a thin layer to the rash as needed for relief from itching and irritation.",
-    },
-    RunnyNose: {
-      medication: "Antihistamines",
-      directions: "Take 1 tablet daily in the evening.",
-    },
-    Sinusitis: {
-      medication: "Nasal Decongestant (e.g., Sudafed)",
-      directions:
-        "Take 1 tablet every 4-6 hours as needed for nasal congestion relief.",
-    },
-    SoreThroat: {
-      medication: "Throat lozenges",
-      directions:
-        "Dissolve one lozenge in your mouth as needed, up to 4 times a day.",
-    },
-    SprainedAnkle: {
-      medication: "Over-the-counter pain reliever (e.g., Ibuprofen)",
-      directions:
-        "Take 1 tablet every 6-8 hours with food for pain and inflammation.",
-    },
-    StomachPain: {
-      medication: "Omee",
-      directions: "Take one tablet early in the morning.",
-    },
-    Sunburn: {
-      medication: "Aloe vera gel",
-      directions:
-        "Apply a thin layer to sunburned skin as needed for soothing relief.",
-    },
-    Toothache: {
-      medication: "Dentalgel",
-      directions:
-        "Apply a small amount directly to the affected tooth or gum as needed.",
-    },
-  };
+  
 
   const checkSymptoms = () => {
     const enteredSymptoms = symptomsInput
@@ -265,7 +114,7 @@ function App() {
           <TextField
             fullWidth
             variant="outlined"
-            placeholder="Enter your symptoms"
+            placeholder="Enter your symptoms . Eg: fever, cold etc."
             value={symptomsInput}
             onChange={(e) => setSymptomsInput(e.target.value)}
             onKeyDown={handleKeyDown}
@@ -283,6 +132,7 @@ function App() {
               </MenuItem>
             ))}
           </TextField>
+          
         </Box>
         <Box display="flex" gap={1} justifyContent="center" mb={2}>
           {isCheckButtonVisible ? (
